bc2030004275fa28fcbdb6d8322aee3ace81b123
Vinícius Azevedo <vinicius.azevedo@ccc.ufcg.edu.br>
2020-11-07 17:08:49 -0300

    Update comments

--- src/main/java/com/vinicius/todo/service/TaskService.java
+++ src/main/java/com/vinicius/todo/service/TaskService.java
@@ -10,10 +10,10 @@ import java.util.Optional;
 
 @Service // define o service
 public class TaskService {
-    private TaskRepository taskRepository; // é a interface que contém os métodos a serem implementados
+    private TaskRepository taskRepository; // objeto que é a interface que contém os métodos a serem implementados
 
     public TaskService(TaskRepository taskRepository) {
-        this.taskRepository = taskRepository; // aqui o spring vai escrever o conteúdo dos métodos da interface TaskRepository
+        this.taskRepository = taskRepository; // aqui o spring vai escrever o conteúdo dos métodos da interface de TaskRepository
     }
 
     public List<Task> list() { // método p/ listar todas as atividades
@@ -39,7 +39,7 @@ public class TaskService {
 
         Optional<Task> op = this.taskRepository.findById(task.getId()); // op -> procura o id da tarefa existente no bd (antiga)
         if (op.isPresent()) { // se for achado
-            Task tUpdate = op.get(); // tUpdate = conteúdo da tarefa antiga tarefa antiga
+            Task tUpdate = op.get(); // tUpdate = conteúdo da tarefa antiga
             tUpdate.setTitle(task.getTitle()); // muda o titulo para o novo
             tUpdate.setDescription(task.getDescription()); // muda a descrição para o novo
             return this.taskRepository.save(tUpdate); // salva a tarefa atualizada
